e1336eb5668c2ce6c8f7d468a3439737b1a3e3d5
Rabin Joshi <joshirabin123@gmail.com>
2026-07-26 16:35:01 +0545

    add application model

---
+++ saathghar_api/src/models/application.model.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+import { ApplicationTypes } from "../types/application.types";
+
+export interface IApplication extends Omit<ApplicationTypes, "listing" | "applicant">, mongoose.Document {
+    _id: mongoose.Types.ObjectId;
+    listing: mongoose.Types.ObjectId;
+    applicant: mongoose.Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const ApplicationMongoSchema = new Schema<IApplication>(
+    {
+        listing: { type: Schema.Types.ObjectId, ref: "Item", required: true },
+        applicant: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        message: { type: String, required: true },
+        status: { type: String, enum: ["pending", "accepted", "rejected"], default: "pending" },
+    },
+    { timestamps: true }
+);
+
+ApplicationMongoSchema.index({ listing: 1, applicant: 1 }, { unique: true });
+
+export const ApplicationModel = mongoose.model<IApplication>("Application", ApplicationMongoSchema);
